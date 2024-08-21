cbc759094ea29d8baa7550bc7b6192f02028f0bb
MUHAMMAD ZOHAIB <muhammadzohaib21122@gmail.com>
2024-08-21 11:50:49 +0500

    Update F.Method.js

--- F.Method.js
+++ F.Method.js
@@ -11,3 +11,17 @@ const arrow = (a, b)=> {
     console.log("Multiply is = ",a * b)
 }
 arrow(2,10);
+
+//Higher order function 
+
+function callbackFunction(){
+    console.log('I am  a callback function');
+}
+
+// higher order function
+function higherOrderFunction(func){
+    console.log('I am higher order function')
+    func()
+}
+
+higherOrderFunction(callbackFunction);
